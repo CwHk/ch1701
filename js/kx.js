8fa2f5583c38c8add02d40a625ccbccf4322d923
陈伟 <2227538634@qq.com>
2017-07-11 01:36:25 +0800

    banner

--- js/kx.js
+++ js/kx.js
@@ -1,3 +1,10 @@
 $(function() {
+    var mySwiper = new Swiper('.swiper-container', {
+        loop: true,
+        autoplay: 30000,
+        // 如果需要分页器
+        pagination: '.swiper-pagination',
+        paginationClickable: true
+    })
 
 })
